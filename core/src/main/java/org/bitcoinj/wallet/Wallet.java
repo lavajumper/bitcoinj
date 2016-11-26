a4a68baec009536e8a4d7bbd2a6d35f8d739987f
Manfred Karrer <mk@nucleo.io>
2016-11-26 18:16:12 +0100

    Wallet: Add printAllPubKeysAsHex()
    Cherry pick https://github.com/bisq-network/bitcoinj/commit/51aadee087b534d958ec484aab1be25fd6f4b35f

--- core/src/main/java/org/bitcoinj/wallet/Wallet.java
+++ core/src/main/java/org/bitcoinj/wallet/Wallet.java
@@ -3338,6 +3338,10 @@ public class Wallet extends BaseTaggableObject
         }
     }
 
+    public String printAllPubKeysAsHex() {
+        return keyChainGroup.printAllPubKeysAsHex();
+    }
+
     @Override
     public String toString() {
         return toString(false, false, null, true, true, null);
